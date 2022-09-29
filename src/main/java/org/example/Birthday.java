37aab533b10e71111717fc7fd17de0d63d81a0df
alexey-gnezdilov <gnezdilov_alexey_1993@mail.ru>
2022-09-30 02:45:53 +0800

    Дз по датам

--- src/main/java/org/example/Birthday.java
+++ src/main/java/org/example/Birthday.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class Birthday {
@@ -15,7 +14,6 @@ public class Birthday {
     public static LocalDate nextBirthday(int year, int month, int date) {
         LocalDate birthDay = LocalDate.of(year,month,date);
         LocalDate today = LocalDate.now();
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         while (birthDay.isBefore(today)) {
             birthDay = birthDay.plusDays(1000);
         }
