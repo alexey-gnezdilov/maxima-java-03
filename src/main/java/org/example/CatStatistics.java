ffbf76ec5af5779b28bbba87d2e832f6097d7072
alexey-gnezdilov <gnezdilov_alexey_1993@mail.ru>
2022-11-03 14:32:24 +0800

    Stream API, лямбда оператор и ссылки на методы - исправления

--- src/main/java/org/example/CatStatistics.java
+++ src/main/java/org/example/CatStatistics.java
@@ -9,21 +9,21 @@ import java.util.stream.Stream;
 
 public class CatStatistics {
 
-    public static ArrayList<Cat> sortByNameAscending(ArrayList<Cat> cats) {
+    public static List<Cat> sortByNameAscending(List<Cat> cats) {
         return cats.
                 stream().
                 sorted(Comparator.comparing(Cat::getName)).
                 collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public static ArrayList<Cat> sortByWeightDescending(ArrayList<Cat> cats) {
+    public static List<Cat> sortByWeightDescending(List<Cat> cats) {
         return cats.
                 stream().
                 sorted(Comparator.comparing(Cat::getWeight).reversed()).
                 collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public static ArrayList<Cat> removeFirstAndLast (ArrayList<Cat> cats) {
+    public static List<Cat> removeFirstAndLast (List<Cat> cats) {
         return cats.
                 stream().
                 skip(1).
@@ -31,7 +31,7 @@ public class CatStatistics {
                 collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public static Cat findFirstNonAngryCat (ArrayList<Cat> cats) {
+    public static Cat findFirstNonAngryCat (List<Cat> cats) {
         return cats.
                 stream().
                 dropWhile(Cat::isAngry).
@@ -39,7 +39,7 @@ public class CatStatistics {
                 orElse(null);
     }
 
-    public static int getCommonWeight(ArrayList<Cat> cats, boolean onlyAngry) {
+    public static int getCommonWeight(List<Cat> cats, boolean onlyAngry) {
         Stream<Cat> finalCats = onlyAngry ? cats.stream().filter(Cat::isAngry) : cats.stream();
         return finalCats.map(Cat::getWeight).reduce(Integer::sum).orElse(0);
 
@@ -57,10 +57,17 @@ public class CatStatistics {
 //                Integer::sum);
     }
 
-    public static Map<String, List<Cat>> groupCatsByFirstLetter (ArrayList<Cat> cats) {
+    public static Map<String, List<Cat>> groupCatsByFirstLetter (List<Cat> cats) {
         return cats
                 .stream()
                 .sorted(Comparator.comparing(Cat::getName))
                 .collect(Collectors.groupingBy(Cat::getFirstLetterInName));
+    }
+
+    public static Map<String, List<Cat>> groupCatsByFirstLetter2 (List<Cat> cats) {
+        return cats
+                .stream()
+                .sorted(Comparator.comparing(Cat::getName))
+                .collect(Collectors.groupingBy(s -> s.getName().substring(0,1)));
     }
 }
